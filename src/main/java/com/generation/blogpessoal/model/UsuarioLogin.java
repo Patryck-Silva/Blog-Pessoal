e2073a2a2fc179560a3ad96ab9e3dd379d0daa66
Patryck Silva <patrycksilva70@gmail.com>
2022-05-13 16:21:12 -0300

    Concertando o método construtor no qual faltava o atributo token

--- src/main/java/com/generation/blogpessoal/model/UsuarioLogin.java
+++ src/main/java/com/generation/blogpessoal/model/UsuarioLogin.java
@@ -18,16 +18,18 @@ public class UsuarioLogin {
 	
 // Primeiro método Construtor
 
-		public UsuarioLogin(Long id, String nome, String usuario, String senha, String foto) {
+		public UsuarioLogin(Long id, String nome, String usuario, String senha, String foto, String token) {
 			this.id = id;
 			this.nome = nome;
 			this.usuario = usuario;
 			this.senha = senha;
 			this.foto = foto;
+			this.token = token;
 		}
 
 // Segundo método Construtor
 		public UsuarioLogin() {	}
+		
 //getters and setters
 	public Long getId() {
 		return id;
