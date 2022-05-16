b37c55b6a0af11540a5c0c784d8f48755e813e2b
Patryck Silva <patrycksilva70@gmail.com>
2022-05-16 16:50:40 -0300

    Adicionando os arquivos do deploy

--- src/main/java/com/generation/blogpessoal/model/Tema.java
+++ src/main/java/com/generation/blogpessoal/model/Tema.java
@@ -1,6 +1,7 @@
 package com.generation.blogpessoal.model;
 
 import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,49 +10,49 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@Entity //create table
-@Table(name = "tb_tema")
-public class Tema {
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-private Long id;
+@Entity
+@Table(name = "tb_temas")
+public class Tema{
+	    
+	    @Id	
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private Long id;
+		
+		@NotBlank(message = "O atributo Descrição é obrigatório e não pode conter espaços em branco")
+		private String descricao;
+		
+		@OneToMany(mappedBy = "tema", cascade = CascadeType.REMOVE)
+		@JsonIgnoreProperties("tema")
+		private List<Postagem> postagem;
+		
+		public Long getId() {
+			return id;
+		}
 
+		public void setId(Long id) {
+			this.id = id;
+		}
 
-@NotBlank(message="a descricao Deve ser preenchido")
-@Size(min = 10, max=200,message = "O texto deve conter no minímo 10 e no máximo 200 caracteres.")
-private String descricao;
+		public String getDescricao() {
+			return descricao;
+		}
 
-//relacionamento
-@OneToMany(mappedBy ="tema",cascade = CascadeType.REMOVE)
-@JsonIgnoreProperties("tema")
-private List<Postagem> postagem;
+		public void setDescricao(String descricao) {
+			this.descricao = descricao;
+		}
 
-public long getId() {
-	return id;
-}
+		/**
+		 *  Métodos Get e Set para a Collection postagem
+		 */
+		public List<Postagem> getPostagem() {
+			return postagem;
+		}
 
-public void setId(long id) {
-	this.id = id;
-}
-
-public String getDescricao() {
-	return descricao;
-}
-
-public void setDescricao(String descricao) {
-	this.descricao = descricao;
-}
-
-public List<Postagem> getPostagem() {
-	return postagem;
-}
-
-public void setPostagem(List<Postagem> postagem) {
-	this.postagem = postagem;
-}
-
+		public void setPostagem(List<Postagem> postagem) {
+			this.postagem = postagem;
+		}
+		
 }
